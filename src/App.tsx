66a898a71118a4f3dec4ea837247c37644851834
atmnsh <kapusta.elizaveta73@gmail.com>
2025-03-04 17:21:18 +0200

    signup and login

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom"
-import { WiseMap } from "@/pages"
+import { SignUp, WiseMap, Login } from "@/pages"
 
 function App() {
   return (
     <Routes>
       <Route element={<WiseMap />} path="/" />
+      <Route element={<SignUp />} path="/sign-up" />
+      <Route element={<Login />} path = "/login"/>
     </Routes>
   )
 }
